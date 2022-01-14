657dc104cf08a61090140c66bcc644416804b7b6
Valentyna-K <kvalentyna@hotmail.com>
2022-01-14 20:24:08 +0200

    Добавила в класс ApplicationDbContext свойства, недостающие в нашем проекте

--- Private_collections/Data/ApplicationDbContext.cs
+++ Private_collections/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Private_collections.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +13,10 @@ namespace Private_collections.Data
             : base(options)
         {
         }
+        public DbSet<Theme> Themes { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Collection> Collections { get; set; }
+        public DbSet<Like> Likes { get; set; }
     }
 }
